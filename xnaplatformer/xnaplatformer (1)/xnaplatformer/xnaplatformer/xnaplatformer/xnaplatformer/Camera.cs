7bf8619d27105d1e1e08db12bdbbbdc4ca7674da
TomC27 <tomclaypool@outlook.com>
2014-05-15 21:17:54 -0700

    fixed minor bugs
    nothing big

---
+++ xnaplatformer/xnaplatformer (1)/xnaplatformer/xnaplatformer/xnaplatformer/xnaplatformer/Camera.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace xnaplatformer
+{
+    class Camera
+    {
+        private static Camera instance;
+        Vector2 position;
+        Matrix viewMatrix;
+
+        public Matrix ViewMatrix
+        {
+            get { return viewMatrix; }
+        }
+        public static Camera Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new Camera();
+                return instance;
+            }
+        }
+
+        public void setFocalPoint(Vector2 focalPosition)
+        {
+            position = new Vector2(focalPosition.X - ScreenManager.Instance.Dimensions.X / 2, focalPosition.Y - ScreenManager.Instance.Dimensions.Y / 2);
+            if (position.X < 0)
+                position.X = 0;
+            if (position.Y < 0)
+                position.Y = 0;
+        }
+
+        public void update()
+        {
+            viewMatrix = Matrix.CreateTranslation(new Vector3(-position, 0));
+        }
+    }
+}
---
+++ xnaplatformer/xnaplatformer (1)/xnaplatformer/xnaplatformer/xnaplatformer/xnaplatformer/ShortSwordBlock.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace xnaplatformer
+{
+    class ShortSwordBlock : AttackAnimation
+    {
+
+
+        public override void LoadContent(ContentManager Content)
+        {
+            content = new ContentManager(Content.ServiceProvider, "Content");
+            image = content.Load<Texture2D>("ShortSwordBlock");
+            position = Vector2.Zero;
+            drawColor = Color.White;
+            frameCounter = 0;
+            switchFrame = 50;
+            currentFrame = new Vector2(0, 0);
+            rotation = 0.0f;
+            axis = 0.0f;
+            scale = alpha = 1.0f;
+            isActive = false;
+            text = string.Empty;
+            frameDimensions = new List<Vector2>();
+
+            frameDimensions.Add(new Vector2(22, 32));
+
+            sourceRect = new Rectangle(SourceX(), 0, (int)frameDimensions[(int)currentFrame.X].X, (int)frameDimensions[(int)currentFrame.X].Y);
+        }
+
+        public override void UnloadContent()
+        {
+            base.UnloadContent();
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            if (effect == SpriteEffects.FlipHorizontally)
+            {
+                offset = new Vector2(-4, 0);
+            }
+
+            else
+            {
+                offset = new Vector2(-2, 0);
+            }
+
+        }
+
+
+    }
+}
